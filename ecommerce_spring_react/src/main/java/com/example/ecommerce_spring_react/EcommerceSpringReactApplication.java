4338f4182eea08149ffedd736a369292a84e2d67
gabrielrodris <biel79082157@gmail.com>
2025-08-14 16:51:25 -0300

    'criando_back-end'

---
+++ ecommerce_spring_react/src/main/java/com/example/ecommerce_spring_react/EcommerceSpringReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_spring_react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSpringReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceSpringReactApplication.class, args);
+	}
+
+}
